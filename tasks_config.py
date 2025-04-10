e04d9ce3a4b2adb75210c86b4bae720b8cad77d0
gteyhyri <ghost.a24@mail.ru>
2025-04-10 23:40:52 +0300

    Update tasks_config.py

--- tasks_config.py
+++ tasks_config.py
@@ -1,22 +1,18 @@
 # Конфигурация заданий
-TASKS = [
-
-    {
-        "id": 1,
-        "title": "ДЕНЬГИ В  TONE",
-        "channel_id": "-1002435743499",
-        "channel_link": "t.me/moneyiston",
-        "reward": 500
+TASKS = {
+    '1': {
+        'id': '1',
+        'title': 'ДЕНЬГИ В TONE',
+        'channel_id': '-1002435743499',
+        'channel_link': 't.me/moneyiston',
+        'reward': 500
     }
-]
+}
 
 # Функция для получения задания по ID
 def get_task_by_id(task_id):
-    for task in TASKS:
-        if task["id"] == task_id:
-            return task
-    return None
+    return TASKS.get(str(task_id))
 
 # Функция для получения всех заданий
 def get_all_tasks():
-    return TASKS 
+    return list(TASKS.values()) 
